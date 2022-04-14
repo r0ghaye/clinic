775914c1c36e70a0a48fd592488c6d9949edca47
saghar abazari <saghi.abazari@gmail.com>
2022-04-14 17:08:26 +0100

    add all files

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,6 @@
 const usernameInput = document.querySelector(".specifications__input");
 const userLastInput = document.querySelector(".last-input");
-const nameMsg = document.querySelector(".name-msg");
+const nameMsg = document.querySelector(".name-msg")
 const lastNamemsg = document.querySelector(".lastname-msg");
 const contactInput = document.querySelector(".contact__input");
 const emailMsg = document.querySelector(".email-msg")
@@ -10,32 +10,37 @@ const inquiryInput = document.querySelector(".inquiry__input");
 const inquiryMsg = document.querySelector(".inquriy-msg");
 const buttonInput = document.querySelector(".specifications__btn");
 const singBtn = document.querySelector(".sing-msg");
+const form = document.querySelector(".contact__form")
 
 
 
 buttonInput.addEventListener("click", singIn);
 function singIn(event) {
     event.preventDefault();
-    //nameMsg.innerText = "";
+    nameMsg.innerText = "";
     lastNamemsg.innerText = "";
     emailMsg .innerText = "";
-    //const usernameValue = usernameInput.value;
+    const usernameValue = usernameInput.value;
+    console.log(usernameValue);
     const userLastValue = userLastInput.value;
     const contactValue  = contactInput.value;
     const subjectCont= subjectContact.value;
     const inquiryValue = inquiryInput.value;
 
     let ifsendData = true;
-
-
-    //if (usernameValue.length === 0) {
-        //nameMsg.innerText = "Please enter your name";
-        //ifsendData = false;
-    //}
+    
+    if (usernameValue.length === 0) {
+        nameMsg.innerText = "Please enter your name";
+        ifsendData = false;
+    } else {
+        nameMsg.innerText = null;
+    }
     
     if (userLastValue.length === 0) {
-        lastNamemsg.innerText = "Please enter your name and your Lastname";
+        lastNamemsg.innerText = "Please enter your Lastname";
         ifsendData = false;
+    } else {
+        lastNamemsg.innerText = null;
     }
 
     if (contactValue.length === 0 || contactValue.indexOf("@") === -1 ||contactValue.indexOf(".") === -1 ) {
@@ -46,21 +51,27 @@ function singIn(event) {
     if (subjectCont.length === 0) {
         subJectmsg.innerText = "Please enter your message";
         ifsendData = false;
+    } else {
+        subJectmsg.innerText = null;
     }
 
     if (inquiryValue.length === 0) {
         inquiryMsg.innerText = "Please enter your inquiry message";
         ifsendData = false;
+        
     } else if (inquiryValue.length <= 10) {
         inquiryMsg.innerText = "your message is too short";
          ifsendData = false;
+
+    } else {
+        inquiryMsg.innerText = null;
     }
 
 
     
     if (ifsendData) {
         const body = JSON.stringify({
-            //username: usernameValue,
+            username: usernameValue,
             lastname: userLastValue,
             email: contactValue,
             message: subjectCont,
