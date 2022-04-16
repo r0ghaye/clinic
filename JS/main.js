90fee7b4b608faca92ecf152e204fdd9c1c2f144
saghar abazari <saghi.abazari@gmail.com>
2022-04-16 11:40:53 +0100

    add all files

---
+++ JS/main.js
@@ -0,0 +1,6 @@
+const collapsibles = document.querySelectorAll(".collapsible");
+collapsibles.forEach((item) =>
+  item.addEventListener("click", function () {
+    this.classList.toggle("collapsible--expanded");
+  })
+);
